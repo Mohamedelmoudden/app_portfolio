00ad2bfc14589bb9d3d8185cab41cbbe9dc77efd
Mohamedelmoudden <mohamedelmoudden17@gmail.com>
2019-08-07 14:25:11 +0200

    first commit

---
+++ src/Components/landingpage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Langing extends Component{
+	render(){
+		return (
+		<div>
+			<h1>Langing Page</h1>
+		</div>
+		);
+	}
+}
+
+export default Langing;
